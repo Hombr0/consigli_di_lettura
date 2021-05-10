aec03492d15be30c60e2ae4e014f8854e5fe334c
Giovanni <giovi.spallanzani@gmail.com>
2021-05-10 14:14:20 +0200

    fixes

--- public/js/prezzi.js
+++ public/js/prezzi.js
@@ -1,6 +1,6 @@
 void( () => {
     const button = document.querySelector("div.button")
-    const url = window.location.href + ":8080"
+    const url = window.location.href + ":8080/"
     button.addEventListener("click", () => {
         document.location.href = url
     })
