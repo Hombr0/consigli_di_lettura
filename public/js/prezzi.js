1578cf20d05065dfe80821545bb51edf273a79ab
Giovanni <giovi.spallanzani@gmail.com>
2021-05-10 14:24:56 +0200

    fixes

--- public/js/prezzi.js
+++ public/js/prezzi.js
@@ -1,6 +1,6 @@
 void( () => {
     const button = document.querySelector("div.button")
-    const url = window.location.href + ":8080/"
+    const url = "https://consigli-di-lettura.herokuapp.com/"
     button.addEventListener("click", () => {
         document.location.href = url
     })
