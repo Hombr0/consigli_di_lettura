77fc09667d5a10141c40757f69aaf8dcefbf853a
Giovanni <giovi.spallanzani@gmail.com>
2021-05-12 22:26:55 +0200

    fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 // const puppeteer = require("puppeteer")
-const rp = require('request-promise')
+const axios = require('axios')
 const cheerio = require('cheerio')
 const util = require("util")
 const fs = require("fs")
@@ -9,6 +9,7 @@ const ejs = require("ejs")
 const write = util.promisify(fs.writeFile)
 const read = util.promisify(fs.readFile)
 const deleteFile = util.promisify(fs.unlink)
+
 const app = express()
 const port = process.env.PORT || 80
 
@@ -29,7 +30,15 @@ const port = process.env.PORT || 80
 //     const result = parseFloat(price.innerHTML.slice(2, 6).replace(',', '.'))
 //     return result
 // }
-const headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64;     x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate",     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+const getRequest = (url) => {
+    axios.get(url, {
+        headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+    }})
+    .catch( err => {
+        console.log(err)
+    })
+}
 
 const Amazon = (title, url, html) => {
     const $ = cheerio.load(html)
@@ -84,20 +93,9 @@ app.post("/titolo", (req, res) => {
     // scraper(urlAmazon, Amazon, title, "Amazon")
     // ])
     Promise.all([
-        rp(urlAmazon, {
-    'authority': 'scrapeme.live',
-    'dnt': '1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'sec-fetch-site': 'none',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-user': '?1',
-    'sec-fetch-dest': 'document',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        }),
-        rp(urlFeltrinelli),
-        rp(urlLibraccio)
+        getRequest(urlAmazon),
+        getRequest(urlFeltrinelli),
+        getRequest(urlLibraccio)
     ])
     .then( data => {
         const dataPrices = [
