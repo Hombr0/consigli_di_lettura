f6fbe23fea0fdd8be62359c379dfbf0702342520
Giovanni <giovi.spallanzani@gmail.com>
2021-05-12 22:58:37 +0200

    fixes

--- server.js
+++ server.js
@@ -85,10 +85,7 @@ app.post("/titolo", (req, res) => {
     // ])
 
     axios.all[
-        axios.get(urlAmazon,  {
-            headers: {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
-        }}),
+        axios.get(urlAmazon),
         axios.get(urlFeltrinelli),
         axios.get(urlLibraccio)
     ]
