33acd475033b820ef496bfb1d36c0e985e68bb59
Giovanni <giovi.spallanzani@gmail.com>
2021-05-12 21:44:37 +0200

    fixes

--- server.js
+++ server.js
@@ -83,7 +83,16 @@ app.post("/titolo", (req, res) => {
     // ])
     Promise.all([
         rp(urlAmazon, {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+            'authority': 'scrapeme.live',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
         }),
         rp(urlFeltrinelli),
         rp(urlLibraccio)
